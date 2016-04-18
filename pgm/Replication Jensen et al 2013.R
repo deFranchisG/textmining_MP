0f4f10ab8d4c718c8001bbc6800315be3b3c0101
PetitSeb <sebastien.turban+github@gmail.com>
2016-04-19 01:16:10 +0200

    Calcul du chi2 sur les trigrams

--- pgm/Replication Jensen et al 2013.R
+++ pgm/Replication Jensen et al 2013.R
@@ -9,7 +9,15 @@ gc()
 
 # libraries
 library(tm) # Text mining
-library(ggplot2)
+library(RWeka) # Trigrames
+
+library(xlsx) # export Excel
+
+library(ggplot2) # Graphique
+library(wesanderson) # Palette graphique
+pal <- wes_palette("GrandBudapest", 2)
+
+
 
 # Initiation de la randomization
 set.seed(142)  
@@ -162,3 +170,110 @@ partis.bin[which(partis%in%ps)] <- -1
 
 # Les betas
 betas <- partis.bin %*% dtm.deputes.sc
+
+# Calculer la mesure de polarisation globale
+betas.df <- data.frame(t(betas))
+names(betas.df) <- "beta"
+data.chi2.df <- data.frame(data.chi2)
+
+beta.chi2.df <- merge(betas.df, data.chi2.df, by = "row.names", 
+                      all.x = T, all.y = T)
+beta.chi2.df$freqtot <- beta.chi2.df$freq.ps + beta.chi2.df$freq.ump
+scorepol <- weighted.mean(beta.chi2.df$beta, w = beta.chi2.df$freqtot)
+
+
+# Trigrams ----------------------------------------------------------------
+
+# Corpus par députés
+
+#Aggréger les textes par le même député
+json.data.df.depute <- subset(json.data.df, 
+                              select = c("auteur.identite.acteurRef", "auteur.groupe.abrege", 
+                                         "textesQuestion.texteQuestion.texte"))
+
+json.data.df.depute <- aggregate(
+  textesQuestion.texteQuestion.texte ~ auteur.identite.acteurRef + auteur.groupe.abrege, 
+  data=json.data.df.depute, 
+  FUN=paste, collapse=' '
+)
+
+# Le texte des questions
+textdata <- json.data.df.depute$textesQuestion.texteQuestion.texte
+Encoding(textdata) <- "UTF-8"
+
+# Transformation en corpus
+mycorpus <- Corpus(VectorSource(textdata))
+
+
+# Cleaning du texte, depuis 
+# https://github.com/cuche27/campagnePresidentielle/blob/master/campagnePresidentielle.R
+
+# Mettre en minuscules
+mycorpus <- tm_map(mycorpus, content_transformer(tolower))
+
+# Remplacer les ponctuations par des espaces. tm fournit la fonction
+# removePunctuation pour ça, mais elle gère mal les apostrophes.
+mycorpus <- tm_map(mycorpus, 
+                   content_transformer(function(x) gsub("\\W", " ", x)))
+
+# Supprimer les mots courants
+mycorpus <- tm_map(mycorpus, removeWords, words = stopwords("french"))
+
+# Supprimer les terminaisons des mots (ex : e, es, ent...)
+mycorpus <- tm_map(mycorpus, stemDocument, language = "french")
+
+# Supprimer les nombres
+mycorpus <- tm_map(mycorpus, removeNumbers)
+
+# Supprimer les espaces superflus
+mycorpus <- tm_map(mycorpus, stripWhitespace)
+
+TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+dtm <- DocumentTermMatrix(mycorpus, control = list(tokenize = TrigramTokenizer))
+
+# Codage des partis droite gauche
+partis <- json.data.df.depute$auteur.groupe.abrege
+
+# Les groupes
+ump <- c("LES-REP", "UMP")
+ps <- c("SRC")
+
+
+# Fréquence et ani-fréquence pour chaque mot, UMP
+row.ump <- which(partis %in% ump)
+freq.ump <- colSums(as.matrix(dtm)[row.ump, ])
+antifreq.ump <- sum(freq.ump) - freq.ump
+
+# même chose pour le ps
+row.ps <- which(partis %in% ps)
+freq.ps <- colSums(as.matrix(dtm)[row.ps, ])
+antifreq.ps <- sum(freq.ps) - freq.ps
+
+# Calcul du chi2
+data.chi2 <- cbind(freq.ump, antifreq.ump, freq.ps, antifreq.ps)
+data.chi2 <- data.frame(data.chi2)
+
+data.chi2$chi2 <- (data.chi2$freq.ump * data.chi2$antifreq.ps - data.chi2$freq.ps * data.chi2$antifreq.ump)^2/
+  ((data.chi2$freq.ump + data.chi2$freq.ps)*(data.chi2$freq.ump + data.chi2$antifreq.ump)*
+     (data.chi2$freq.ps + data.chi2$antifreq.ps)*(data.chi2$antifreq.ump + data.chi2$antifreq.ps))
+
+# Regarder quels mots ont le plus large chi2
+data.chi2 <- data.chi2[order(- data.chi2$chi2), ]
+
+# Rajouter une couleur pour savoir qui a utilisé le mot le plus souvent
+data.chi2$indexparti <- "Droite"
+data.chi2$indexparti[which(data.chi2$freq.ps/(data.chi2$freq.ps+data.chi2$antifreq.ps) >
+                             data.chi2$freq.ump/(data.chi2$freq.ump+data.chi2$antifreq.ump))] <- "Gauche"
+
+data.chi2.top <- data.chi2[1:50, ]
+
+p <- ggplot(data.chi2.top, aes(reorder(row.names(data.chi2.top), chi2), chi2))
+p <- p + geom_bar(aes(fill = indexparti), stat = "identity")
+p <- p + theme_bw()
+p <- p + scale_fill_manual(values = pal, name = "Hémicycle")
+p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+p <- p + xlab("Trigram") + ylab("Score du Chi2")
+p
+
+write.xlsx(data.chi2[1:200, ], file = "tbl\\Trigrams prononces par les partis par chi2.xlsx")
+
